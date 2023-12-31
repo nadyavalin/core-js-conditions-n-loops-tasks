a585103f9053dd8e91328cfc15389c34449e8b1b
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2023-12-31 23:51:46 +0300

    fix: make proper code for the function isIsoscelesTriangle

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -99,6 +99,9 @@ function isIsoscelesTriangle(a, b, c) {
   if (a <= 0 || b <= 0 || c <= 0) {
     return false;
   }
+  if (a + b < c || b + c < a || c + a < b) {
+    return false;
+  }
   return a === b || b === c || a === c;
 }
 
